7a97eb60a76c15e14e0dfc22ebef2a07f9404416
Naim Panjwani <naim.panjwani@gmail.com>
2019-03-29 01:51:43 -0400

    Privatizing token key

--- ldQueries.py
+++ ldQueries.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+import tokens
 
 def parseR2(urltext):
     temp = urltext.strip().split('\n')
@@ -10,7 +11,6 @@ def parseR2(urltext):
             return(float(t.strip().split(':')[1].strip()))
     return(np.nan)
 
-token = 'bc87879cf755'
 lead_snp = 'rs7512462'
 snp_list = ['rs61814953', 'rs1342063']
 european_populations = ['CEU', 'TSI', 'FIN', 'GBR', 'IBS']
@@ -22,7 +22,7 @@ for snp in snp_list:
     snp_query = 'var1=' + lead_snp + '&var2=' + snp
     population_query = '&pop=' + "%2B".join(european_populations) 
     #ld_query = '&r2_d=' + ld_type 
-    token_query = '&token=' + token
+    token_query = '&token=' + tokens.token
     url = base_url + snp_query + population_query + token_query
     response = requests.get(url).text
     r2 = parseR2(response)
