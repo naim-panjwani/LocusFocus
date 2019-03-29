40e05e879ac5eb50f6a169582d5a99aeada14695
Naim Panjwani <naim.panjwani@gmail.com>
2019-03-29 01:47:01 -0400

    Checking feasibility of querying pairwise SNP LD in LDlink's API

---
+++ ldQueries.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+
+def parseR2(urltext):
+    temp = urltext.strip().split('\n')
+    for t in temp:
+        if t.strip().find('R2') != -1:
+            return(float(t.strip().split(':')[1].strip()))
+    return(np.nan)
+
+token = 'bc87879cf755'
+lead_snp = 'rs7512462'
+snp_list = ['rs61814953', 'rs1342063']
+european_populations = ['CEU', 'TSI', 'FIN', 'GBR', 'IBS']
+ld_type = 'r2'
+base_url = 'https://ldlink.nci.nih.gov/LDlinkRest/ldpair?'
+
+r2_pairs = []
+for snp in snp_list:
+    snp_query = 'var1=' + lead_snp + '&var2=' + snp
+    population_query = '&pop=' + "%2B".join(european_populations) 
+    #ld_query = '&r2_d=' + ld_type 
+    token_query = '&token=' + token
+    url = base_url + snp_query + population_query + token_query
+    response = requests.get(url).text
+    r2 = parseR2(response)
+    print(r2)
+    r2_pairs.append(r2)
+    
