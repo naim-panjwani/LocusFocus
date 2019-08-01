9890a5cacf17316fbafce3e2f205cc786e540e68
Naim Panjwani <naim.panjwani@gmail.com>
2019-08-01 11:29:00 -0400

    updated heatmap labels

--- static/js/plot_heatmap.js
+++ static/js/plot_heatmap.js
@@ -3,7 +3,10 @@ function plot_heatmap(genes, tissues, SSPvalues) {
           z: SSPvalues,
           x: genes,
           y: tissues,
-          type: 'heatmap'
+          type: 'heatmap',
+          hovertemplate: 'Gene: %{x}' +
+                         '<br>Tissue: %{y}<br>' +
+                         '-log10(SS P-value): %{z}'
     }];
     Plotly.newPlot('heatmap', data);
 }
