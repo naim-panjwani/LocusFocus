a2b6aff8138a9561bdfd6f3169811d729abb0b22
Naim Panjwani <naim.panjwani@gmail.com>
2019-09-25 17:30:22 -0400

    added simple sum p-values datatable

---
+++ static/js/heatmap_table.js
@@ -0,0 +1,60 @@
+var theTable = d3.select("#variants-table");
+
+function buildTable(genes, tissues, SSPvalues) {
+    // console.log(data);
+    var tbody = d3.select("#variants-table").select("tbody");
+
+    // Clear table:
+    // if ( $.fn.dataTable.isDataTable( '#variants-table' ) ) {
+    //     $('#variants-table').DataTable().destroy();
+    // }
+    tbody.text("")
+
+    // Build column headers:
+    for(i=0; i<tissues.length; i++) {
+        theTable.select('thead').select('tr')
+            .append('th')
+            .attr('class', 'th-sm')
+            .text(tissues[i]);
+    }
+
+    // Add table body:
+    for(i=0; i<genes.length; i++) { // for each tissue
+        var row = tbody.append('tr');
+        row.append('td').text(genes[i]);
+        for(j=0; j<tissues.length; j++) { // for each column
+            row.append('td').text(SSPvalues[j][i]);
+        }
+    }
+    // Add DataTables functionality:
+    varTable = $(document).ready(function () {
+        var thedatatable = $('#variants-table').DataTable({
+            dom: 'Bfrtipl',
+            buttons: [
+                'copy',
+                {
+                    extend: 'csv',
+                    filename: 'SS_pvalues'
+                },
+                {
+                    extend: 'excel',
+                    filename: 'SS_pvalues'
+                },
+                {
+                    extend: 'pdf',
+                    filename: 'SS_pvalues'
+                }
+            ]
+        });
+        // $('.dataTables_length').addClass('bs-select');
+        // new $.fn.dataTable.Buttons( thedatatable, {
+        //     buttons: [
+        //         'copy', 'csv', 'excel', 'pdf'
+        //     ]
+        // });
+        // thedatatable.buttons().container()
+        //     .appendTo( $('.col-sm-6:eq(0)', thedatatable.table().container() ) );
+    });
+    
+}
+
